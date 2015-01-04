208ebe16e48e092602501dfd8bc04e4cbb7a8b83
zeroRooter <williamap@gmail.com>
2015-01-04 14:30:09 -0500

    trouble receiving packets

--- src/Driver.java
+++ src/Driver.java
@@ -5,21 +5,51 @@ import java.util.Scanner;
 
 
 public class Driver {
-	static ArrayList<String> connectedHostNames = new ArrayList<>(10);
+	volatile static ArrayList<String> connectedHostNames = new ArrayList<>(10);//all the people connected; excluding this client
 
 	Messenger messenger;
 	InetAddress myIP;
 	
 	public static void main(String[] args) {
-		Driver main = new Driver();
+		final Driver main = new Driver();
 		main.connect(); // Connect to the Multicast Socket
 		main.sendJoinMessage();
 		
-		new Thread(main.new ReceiveMessages()).start(); //Start the receive message thread
+		//new Thread(main.new ReceiveMessages()).start(); //Start the receive message thread
 		
-		main.mainLoop();
+		//main.mainLoop();
+		//String message = messenger.recieveMessage();
+		//main.addUser(main.joinListener());
+		new Thread(
+		new Runnable(){
+			@Override
+			public void run() {
+				while(true){
+				main.joinListener();
+				}
+				
+			}
+			
+		}).start();
 	}
-	
+	//Listen for broadcast messages sent by people who have joined, and adds that user to connectedHostNames
+	//Return the member who joined
+	public  String joinListener(){	
+		String message = messenger.recieveMessage();
+		System.out.println(message);
+		int endOfHostName = message.indexOf("has joined###!");		
+		String hostName = message.substring(0 , endOfHostName);
+		System.out.println(hostName + "Has joined");
+		addUser(hostName);
+		return hostName;
+	}
+	public void addUser(String user){
+		connectedHostNames.add(user);
+	}
+	public void removeUser(String user){
+		connectedHostNames.remove(user);
+	}
+	/*
 	public void mainLoop(){
 		while(messenger.isConnected()){
 			Scanner s = new Scanner(System.in);
@@ -27,7 +57,7 @@ public class Driver {
 			messenger.sendMessage(msg);
 		}
 		System.out.println("You have quit");
-	}
+	}*/
 	public void connect(){
 		messenger = new Messenger("225.5.5.5", 6789);
 		System.out.println("messenger port: " + messenger.getMultiSocket().getPort() + " Local port:" + messenger.getMultiSocket().getLocalPort() + " address: " + messenger.getMultiSocket().getInetAddress().getHostAddress() );
@@ -37,13 +67,13 @@ public class Driver {
 		try {
 			myIP = InetAddress.getLocalHost();
 			messenger.sendMessage(myIP.getHostName() + "has joined###!");
-			connectedHostNames.add(myIP.getHostName());
+			//connectedHostNames.add(myIP.getHostName());
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+	/*
 	public class ReceiveMessages implements Runnable{
 
 		@Override
@@ -68,9 +98,15 @@ public class Driver {
 				else{ // print the message;
 					System.out.println(message);
 				}
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
 		
-	}
+	}*/
 
 }
